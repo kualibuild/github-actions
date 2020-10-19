d698fe6be87524641445ff20ea2b99efcd29f22a
Dallin Osmun <dosmun@gmail.com>
2020-10-19 14:24:45 -0600

    maybe github-actions don't support async function *

--- purge-artifacts/index.js
+++ purge-artifacts/index.js
@@ -3,37 +3,38 @@ const github = require('@actions/github')
 
 const { owner, repo } = github.context.repo
 
-async function * artifacts (octokit) {
-  let hasMore = false
-  let page = 1
-  do {
-    const args = { owner, repo, page, per_page: 100 }
-    const { data } = await octokit.actions.listArtifactsForRepo(args)
-    hasMore = data.total_count / 100 > page
-    for (const artifact of data.artifacts) yield artifact
-    page++
-  } while (hasMore)
+async function fetchArtifacts (octokit) {
+  const args = { owner, repo, page: 1, per_page: 100 }
+  const res = await octokit.actions.listArtifactsForRepo(args)
+  core.info(`total artifacts: ${res.data.total_count}`)
+  core.info(JSON.stringify(res.data.artifacts[0], null, 2))
+  return res.data.artifacts
 }
 
-async function main () {
+async function purge (octokit, expires) {
+  let purgeCount = 0
+  const artifacts = await fetchArtifacts(octokit)
+  if (!artifacts.length) return 0
+  for (const artifact of artifacts) {
+    const { id, created_at: createdAt } = artifact
+    if (Date.now() - new Date(createdAt).getTime() < expires) return
+    purgeCount++
+    await octokit.actions.deleteArtifact({ owner, repo, artifact_id: id })
+  }
+  return purgeCount + purge(octokit, expires)
+}
+
+async function run () {
   try {
-    core.info('running main')
     const expires = +core.getInput('expires', { required: true })
     const token = core.getInput('token', { required: true })
     const octokit = github.getOctokit(token)
-    let artifactCount = 0
-    core.info('fetching artifacts')
-    for await (const { id, created_at: createdAt } of artifacts(octokit)) {
-      if (Date.now() - new Date(createdAt).getTime() < expires) return
-      artifactCount++
-      await octokit.actions.deleteArtifact({ owner, repo, artifact_id: id })
-    }
-    core.info(`Purged ${artifactCount} artifacts`)
+    const purgeCount = await purge(octokit, expires)
+    core.info(`Purged ${purgeCount} artifacts`)
   } catch (error) {
     core.info('failed')
     core.setFailed(error.message)
   }
 }
 
-core.info('Starting purge action')
-main()
+run()
