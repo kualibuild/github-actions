597a25c3520c5664a963713de6ae5f1bd0bf9b4f
Dallin Osmun <dosmun@gmail.com>
2020-10-19 14:47:41 -0600

    loop through artifacts backwards (because we are removing some)

--- purge-artifacts/index.js
+++ purge-artifacts/index.js
@@ -3,25 +3,29 @@ const github = require('@actions/github')
 
 const { owner, repo } = github.context.repo
 
-async function fetchArtifacts (octokit) {
-  const args = { owner, repo, page: 1, per_page: 100 }
+async function getLastPage (octokit) {
+  const args = { owner, repo, page: 1, per_page: 1 }
   const res = await octokit.actions.listArtifactsForRepo(args)
-  core.info(`total artifacts: ${res.data.total_count}`)
-  core.info(JSON.stringify(res.data.artifacts[0], null, 2))
-  return res.data.artifacts
+  const lastPage = Math.ceil(res.data.total_count / 100)
+  core.info(`Total Artifact Count: ${res.data.total_count}`)
+  core.info(`Last Page: ${lastPage}`)
+  return lastPage
 }
 
-async function purge (octokit, expires) {
-  let purgeCount = 0
-  const artifacts = await fetchArtifacts(octokit)
-  if (!artifacts.length) return 0
-  for (const artifact of artifacts) {
-    const { id, created_at: createdAt } = artifact
-    if (Date.now() - new Date(createdAt).getTime() < expires) continue
-    purgeCount++
+const ms = a => new Date(a.created_at).getTime()
+
+async function purge (octokit, expires, page) {
+  if (page <= 0) return 0
+  const args = { owner, repo, page, per_page: 100 }
+  const res = await octokit.actions.listArtifactsForRepo(args)
+  const expired = res.data.artifacts.filter(a => ms(a) < expires)
+  core.info(
+    `Fetched page ${page}. Artifact Count is now: ${res.data.total_count}. Expired is: ${expired}`
+  )
+  for (const { id } of expired) {
     await octokit.actions.deleteArtifact({ owner, repo, artifact_id: id })
   }
-  return purgeCount + purge(octokit, expires)
+  return expired.length + (await purge(octokit, expires, page - 1))
 }
 
 async function run () {
@@ -29,7 +33,8 @@ async function run () {
     const expires = +core.getInput('expires', { required: true })
     const token = core.getInput('token', { required: true })
     const octokit = github.getOctokit(token)
-    const purgeCount = await purge(octokit, expires)
+    const lastPage = await getLastPage(octokit)
+    const purgeCount = await purge(octokit, Date.now() - expires, lastPage)
     core.info(`Purged ${purgeCount} artifacts`)
   } catch (error) {
     core.info('failed')
