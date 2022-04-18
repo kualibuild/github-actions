5cbe1269837be75531f4d1b28b56355a047ee2fb
Cameron Larsen <cameron@larsenfam.org>
2022-04-18 09:12:21 -0600

    fix regex for ips

--- argodeploy/close_api.sh
+++ argodeploy/close_api.sh
@@ -8,11 +8,13 @@ if [[ $# -lt 4 ]]; then
   exit 1
 fi
 
+IP_REGEX='^(([0-9]{1,3}.){3}[0-9]{1,3}\/[0-9]{1,2},*){1,}$'
+
 VALID_REGIONS=("us-east-2" "us-east-1" "us-west-1" "us-west-2" "eu-west-1" "eu-west-2" "eu-west-3" "eu-central-1" "ap-south-1" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ap-northeast-2" "ap-northeast-3" "ca-central-1")
 [[ "$1" == "true" || "$1" == "false" ]] || { echo "ERR: First argument must be true or false"; echo "${USAGE}"; exit 1; }
 if [[ "$1" == "true" ]]; then
   [[ "$2" =~ ^[a-zA-Z][-a-zA-Z0-9]*$ ]] || { echo "ERR: EKS cluster_name must be alphanumeric and start with a letter"; echo "${USAGE}"; exit 1; }
-  [[ "$4" =~ ^(\"([0-9]{1,3}.){3}[0-9]\/[0-9]{1,2}\",*){1,}$ ]] || { echo "ERR: original_ips should be one or more ip addresses in CIDR notation quote wrapped and comma delimited"; echo "${USAGE}"; exit 1; }
+  [[ "$4" =~ ${IP_REGEX} ]] || { echo "ERR: original_ips should be one or more ip addresses in CIDR notation comma delimited"; echo "${USAGE}"; exit 1; }
 else
   [[ "$2" =~ ^([a-z0-9\-]*\.){0,2}k8s.local$ ]] || { echo "ERR: KOPS cluster_name must be FQDN ending in k8s.local"; echo "${USAGE}"; exit 1; }
   [[ "$4" =~ ^sg-[a-zA-Z0-9]{17}$ ]] || { echo "ERR: security_group_id should be an 18 charachter string matching \"^sg-[a-zA-Z0-9]{17}$\""; echo "${USAGE}"; exit 1; }
