9251ab5d2acea69b8096e9a0e6bbcb11caa3097b
Cameron Larsen <cameron@larsenfam.org>
2022-04-18 10:38:15 -0600

    only make a PR if there are changes

--- argodeploy/run.sh
+++ argodeploy/run.sh
@@ -30,9 +30,14 @@ if [[ "$1" == "verify" ]]; then
   git switch -c ${1} origin/${1}
   git checkout -b update-${1}-${TAG}
   sed -i "s/^    newTag: .*$/    newTag: \'${TAG}\'/" ./*/app/kustomization.yaml
-  git commit -am "Updated image tag to ${TAG}"
-  echo "Pushing to remote: update-${1}-${TAG}"
-  git push --set-upstream origin update-${1}-${TAG} &>/dev/null
+  # check if commit is needed
+  git add . -A &>/dev/null
+  changes=$(git status -s)
+  if [ -n "${changes}" ]; then 
+    git commit -m "Updated image tag to ${TAG}"
+    echo "Pushing to remote: update-${1}-${TAG}"
+    git push --set-upstream origin update-${1}-${TAG} &>/dev/null
+  fi
 else
   git switch -c verify origin/verify
   git switch ${1}
@@ -43,73 +48,82 @@ else
   for i in $(ls -d */ | grep ${3}); do
     git checkout verify -- ${i}
   done
-  git commit -am "Updated image tag to ${TAG}"
-  echo "Pushing to remote: update-prod-${TAG}"
-  git push --set-upstream origin update-prod-${TAG} &>/dev/null
+  # check if commit is needed
+  git add . -A &>/dev/null
+  changes=$(git status -s)
+  if [ -n "${changes}" ]; then 
+    git commit -am "Updated image tag to ${TAG}"
+    echo "Pushing to remote: update-prod-${TAG}"
+    git push --set-upstream origin update-prod-${TAG} &>/dev/null
+  fi
 fi
 
 # create PR
-export GITHUB_PR=$(hub pull-request -b ${1} -m "Updated image tag to ${TAG}"| rev | cut -d'/' -f1 | rev || { echo "ERR: PR not created"; exit 1; })
-echo "Created PR: ${GITHUB_PR}"
+if [ -n "${changes}" ]; then
+  export GITHUB_PR=$(hub pull-request -b ${1} -m "Updated image tag to ${TAG}"| rev | cut -d'/' -f1 | rev || { echo "ERR: PR not created"; exit 1; })
+  echo "Created PR: ${GITHUB_PR}"
 
-# deal with status checks
-count=0
-until [[ $(hub ci-status) == "pending" ]]; do 
-  ((count+=1))
-  [[ ${count} -gt 1 ]] && echo "No status checks required for this PR" && break
-  echo "Waiting to see if status checks are required for this PR"
-  sleep 5
-done
-count=0
-while [[ $(hub ci-status) == "pending" ]]; do 
-  [[ ${count} -eq 0 ]] && echo "Waiting for required status checks to pass before merge"
-  ((count+=1))
-  sleep 1
-done
-case $(hub ci-status) in
-  success)
-    echo "Status checks passed"
-    ;;
-  failure)
-    echo "Required status checks did not pass. For more information, please see $(hub ci-status --verbose | awk '{print $3}')"; exit 1
-    ;;
-esac
+  # deal with status checks
+  count=0
+  until [[ $(hub ci-status) == "pending" ]]; do 
+    ((count+=1))
+    [[ ${count} -gt 1 ]] && echo "No status checks required for this PR" && break
+    echo "Waiting to see if status checks are required for this PR"
+    sleep 5
+  done
+  count=0
+  while [[ $(hub ci-status) == "pending" ]]; do 
+    [[ ${count} -eq 0 ]] && echo "Waiting for required status checks to pass before merge"
+    ((count+=1))
+    sleep 1
+  done
+  case $(hub ci-status) in
+    success)
+      echo "Status checks passed"
+      ;;
+    failure)
+      echo "Required status checks did not pass. For more information, please see $(hub ci-status --verbose | awk '{print $3}')"; exit 1
+      ;;
+  esac
 
-# merge PR 
-hub api -XPUT "repos/${GITHUB_REPO}/pulls/${GITHUB_PR}/merge" &>/dev/null
+  # merge PR 
+  hub api -XPUT "repos/${GITHUB_REPO}/pulls/${GITHUB_PR}/merge" &>/dev/null
 
-# wait for merge to complete
-until [[ $(hub pr list | grep -c update-${1}-${TAG}) == 0 ]]; do
-  echo "  * waiting for merge to complete. Checking again in 30s"
-  sleep 30
-done
-echo "  * Merged."
+  # wait for merge to complete
+  until [[ $(hub pr list | grep -c update-${1}-${TAG}) == 0 ]]; do
+    echo "  * waiting for merge to complete. Checking again in 30s"
+    sleep 30
+  done
+  echo "  * Merged."
 
-NREV=$(kubectl -n ${2} get deploy ${SERVICE} -o jsonpath='{.metadata.annotations.deployment\.kubernetes\.io/revision}')
-
-# wait for argo to increment the version on the deploy which signals the start of the rollout
-echo "Waiting for rollout to begin for deployment/${SERVICE}."
-count=0
-until [[ $((${NREV} - ${REV})) == 1 ]]; do
-  ((count+=0))
-  [[ ${count} -ge 20 ]] && echo "  * Rollout has yet to begin for deployment/${SERVICE}. Aborting."
-  echo "  * Rollout has yet to begin for deployment/${SERVICE}. Checking again in 30s"
-  sleep 30
   NREV=$(kubectl -n ${2} get deploy ${SERVICE} -o jsonpath='{.metadata.annotations.deployment\.kubernetes\.io/revision}')
-done
 
-# Delete the branch now that we are done with it
-git switch ${1}
-git branch -d update-${1}-${TAG}
-git push origin --delete update-${1}-${TAG}
+  # wait for argo to increment the version on the deploy which signals the start of the rollout
+  echo "Waiting for rollout to begin for deployment/${SERVICE}."
+  count=0
+  until [[ $((${NREV} - ${REV})) == 1 ]]; do
+    ((count+=0))
+    [[ ${count} -ge 20 ]] && echo "  * Rollout has yet to begin for deployment/${SERVICE}. Aborting."
+    echo "  * Rollout has yet to begin for deployment/${SERVICE}. Checking again in 30s"
+    sleep 30
+    NREV=$(kubectl -n ${2} get deploy ${SERVICE} -o jsonpath='{.metadata.annotations.deployment\.kubernetes\.io/revision}')
+  done
 
-kubectl -n ${2} rollout status deploy/$SERVICE
+  # Delete the branch now that we are done with it
+  git switch ${1}
+  git branch -d update-${1}-${TAG}
+  git push origin --delete update-${1}-${TAG}
 
-[[ ${count} -ge 20 ]] && exit 1
+  kubectl -n ${2} rollout status deploy/$SERVICE
 
-# add deploy marker to honeycomb
-[[ ! -z ${HONEYCOMB_KEY} ]] || curl https://api.honeycomb.io/1/markers/builder \
-  -X POST \
-  -H "Content-Type: application/json" \
-  -H "X-Honeycomb-Team: ${HONEYCOMB_KEY}" \
-  -d "{\"message\":\"Deploy ${SERVICE_NAME} ${TAG}\", \"type\":\"deploy-${SERVICE_NAME}\", \"start_time\": ${DEPLOY_START_TIME}, \"end_time\": $(date +%s), \"url\": \"https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}\"}"
+  [[ ${count} -ge 20 ]] && exit 1
+
+  # add deploy marker to honeycomb
+  [[ ! -z ${HONEYCOMB_KEY} ]] || curl https://api.honeycomb.io/1/markers/builder \
+    -X POST \
+    -H "Content-Type: application/json" \
+    -H "X-Honeycomb-Team: ${HONEYCOMB_KEY}" \
+    -d "{\"message\":\"Deploy ${SERVICE_NAME} ${TAG}\", \"type\":\"deploy-${SERVICE_NAME}\", \"start_time\": ${DEPLOY_START_TIME}, \"end_time\": $(date +%s), \"url\": \"https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}\"}"
+else
+  echo "No changes to commit"
+fi
