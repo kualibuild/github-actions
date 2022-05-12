cd5d73da2127e03b8a9f2f9a769129bbd612494e
Cameron Larsen <cameron@larsenfam.org>
2022-05-12 14:52:32 -0600

    update eks-access and smoketest

--- eks-access/get_kubeconfig.sh
+++ eks-access/get_kubeconfig.sh
@@ -8,8 +8,8 @@ if [[ $# -lt 2 ]]; then
 fi
 
 VALID_REGIONS=("us-east-2" "us-east-1" "us-west-1" "us-west-2" "eu-west-1" "eu-west-2" "eu-west-3" "eu-central-1" "ap-south-1" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ap-northeast-2" "ap-northeast-3" "ca-central-1")
+[[ "${1}" =~ ^[a-zA-Z][-a-zA-Z0-9]*$ ]] || { echo "ERR: EKS cluster_name must be alphanumeric and start with a letter"; echo "${USAGE}"; exit 1; }
 [[ "${VALID_REGIONS[@]}" =~ "${2}" ]] || { echo "ERR: region must be one of the following: ${VALID_REGIONS[@]}"; echo "${USAGE}"; exit 1; }
-[[ "$1" =~ ^[a-zA-Z][-a-zA-Z0-9]*$ ]] || { echo "ERR: EKS cluster_name must be alphanumeric and start with a letter"; echo "${USAGE}"; exit 1; }
 
 # enable automatic retrylogic in awscli
 export AWS_RETRY_MODE=standard
