a2560d1e067c77a189143afd8c2195c1f5531569
Cameron Larsen <cameron@larsenfam.org>
2022-06-14 15:24:57 -0600

    update cluster-update to use binary

---
+++ cluster-update/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "Building ${PWD##*/} for linux/amd64..."
+env GOOS=linux GOARCH=amd64 go build -o ${PWD##*/}-linux-amd64 .
+echo "Building ${PWD##*/} for linux/arm64..."
+env GOOS=linux GOARCH=arm64 go build -o ${PWD##*/}-linux-arm64 .
+echo "Building ${PWD##*/} for darwin/amd64..."
+env GOOS=darwin GOARCH=arm64 go build -o ${PWD##*/}-darwin-amd64 .
+echo "Building ${PWD##*/} for darwin/arm64..."
+env GOOS=darwin GOARCH=arm64 go build -o ${PWD##*/}-darwin-arm64 .
--- cluster-update/run.sh
+++ cluster-update/run.sh
@@ -6,4 +6,20 @@ if [[ $# -lt 2 ]]; then
   exit 1
 fi
 
-go run main.go -c ${1} -r ${2}
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "Running on Mac OS"
+  export o=darwin
+else
+  echo "Running on Linux OS"
+  export o=linux
+fi
+
+if [[ "$(uname -m)" == "x86_64" ]]; then
+  echo "Running on 64-bit architecture"
+  export a=amd64
+else
+  echo "Running on 32-bit architecture"
+  export a=arm64
+fi
+
+./${PWD##*/}-${o}-${a} -c ${1} -r ${2}
